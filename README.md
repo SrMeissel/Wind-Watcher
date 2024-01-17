b748472c16c5677f2e6ea4974442610f5fa5d96d
Ethan Mizer <mizerethan32@gmail.com>
2024-01-17 09:35:16 -0500

    Learning MySQL

--- README.md
+++ README.md
@@ -16,3 +16,6 @@ Running graphical apps on wsl requires an third-party X server like 'mDNS'
 
 -DearPyGui <https://github.com/hoffstadt/DearPyGui#installation>
 -Django
+https://twisted.org/
+https://pyinstaller.org/en/stable/
+https://stackoverflow.com/questions/5458048/how-can-i-make-a-python-script-standalone-executable-to-run-without-any-dependen
--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 import dearpygui.dearpygui as dpg
+import mysql.connector 
 
+database = mysql.connector.connect( 
+    host= "localhost",
+    user= "client",
+    password = "meissel",
+    database = "WindWatcher"
+)
+print(database)
+
+cursor = database.cursor()
+
+#cursor.execute("CREATE DATABASE WindWatcher")
+#cursor.execute("SHOW DATABASES")
+
+#cursor.execute("CREATE TABLE Data (DeviceID VARCHAR(255), Temperature VARCHAR(255), Pressure VARCHAR(255))")
+#cursor.execute("ALTER TABLE Data ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY")
+#cursor.execute("SHOW TABLES")
+
+for x in cursor:
+    print(x)
+
+# GUI ================================================
 dpg.create_context()
 dpg.create_viewport(title='Custom Title', width=600, height=300)
 
